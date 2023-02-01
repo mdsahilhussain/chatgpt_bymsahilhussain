436c0b289f9a4d7b33b132874609411c7ad911e9
mdsahilhussain <mdsahil.hussain7@gmail.com>
2023-02-01 21:07:18 +0530

    add env file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
-
+import { Configuration, OpenAIApi } from "openai";
 function App() {
+  const configuration = new Configuration({
+    apiKey:process.env.OPENAI_API_KEY,
+  });
   return <div></div>;
-}
+} 
 
 export default App;
