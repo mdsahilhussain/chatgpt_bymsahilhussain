ba46ff01f7ed3a6b1dc99dcff0c0d237580a580c
mdsahilhussain <mdsahil.hussain7@gmail.com>
2023-02-09 20:58:45 +0530

    new file

---
+++ client/src/components/asider/Asider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Asider() {
+  return (
+    <div></div>
+  )
+}
+
+export default Asider
--- client/src/pages/chatGPT/ChatGPT.jsx
+++ client/src/pages/chatGPT/ChatGPT.jsx
@@ -1,32 +1,17 @@
 import React, { useState } from "react";
 import "./chatgpt.css";
 import sendIcon from "../../assets/send.svg";
+import { Asider } from "../../components";
 function ChatGPT() {
   const [inputValue, setInputValue] = useState("");
   let loadInterval;
 
-  function loader(element) {
-    element.textContent = "";
-    loadInterval = setTimeout(() => {
-      element.textContent += ".";
-      if (element.textContent === "...") {
-        element.textContent = "";
-      }
-    }, 300);
-    console.log(loadInterval);
-  }
-
-  loader();
-
   return (
-    <div id="app">
-      <div id="chat___container"></div>
-      <form>
-        <textarea name="prompt" rows="1" cols="1"></textarea>
-        <button type="submit">
-          <img src={sendIcon} alt="sendIcon" />
-        </button>
-      </form>
+    <div className="container">
+      <aside className="container___sidemenu">
+        <Asider />
+      </aside>
+      <section className="container___chatbox"></section>
     </div>
   );
 }
