15f57edafb0828c19e1372a1fe88d60fc7418b79
mdsahilhussain <mdsahil.hussain7@gmail.com>
2023-02-25 13:18:45 +0530

    delete every thing

--- client/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Asider } from "./asider/Asider";
-export { default as Chatbox } from "./chatbox/Chatbox";
-export { default as Chatinput } from "./chatinput/Chatinput";
--- client/src/pages/index.js
+++
@@ -1,1 +0,0 @@
-export { default as ChatGPT } from "./chatGPT/ChatGPT";
